59f9021d3cb128a11c92911a281a8b59e7569d46
nandi0691 <nandeesh.0691@gmail.com>
2022-02-08 15:24:54 +0000

    BlockChain initial commit

---
+++ Main.h
@@ -0,0 +1,27 @@
+#ifndef __MAIN__H_
+#define __MAIN__H_
+
+#include<iostream>
+#include<string>
+#include<stdio.h>
+#include <algorithm>
+#include <sys/stat.h>
+#include <fcntl.h>    /* For O_RDWR */
+#include <unistd.h> 
+#include <fstream>
+#include <time.h>
+#include <cstring>
+//Header file for main program
+
+//Functions
+void ExecuteCommand(const std::string cmd,std::string & ans);
+bool IsFileExists(const char* fname);
+void CreateFile(const char* file);
+std::string SHA256Enc(std::string tokenToEncrypt);
+void CreateDirectory(const char* path, mode_t mode);
+void recalculateHash(std::string block);
+
+//global variables
+static int nonce = 0;
+static int currBlock = 1;
+#endif
